6ec759730a638d9909804c815a22a1760d99f2e7
JuanRT <juan.m13@hotmail.fr>
2024-12-31 18:42:43 +0100

    Update project header with new project modal

--- client/src/app/projects/projectHeader.tsx
+++ client/src/app/projects/projectHeader.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
-import { Clock, Filter, Grid3x3, Grid3X3, List, Share2, Table } from "lucide-react";
+import { Clock, Filter, Grid3x3, Grid3X3, List, PlusSquare, Share2, Table } from "lucide-react";
 import { useState } from "react";
+import ModalNewProject from "./modalNewProject";
 
 type Props = {
     activeTab: string;
@@ -10,33 +11,48 @@ type Props = {
 const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
     const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
     return (
-        /** Modal New Project */
-        <div className="px-4 xl:px-6">
-            <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-                <Header name="Product Design Development" />
-            </div>
-            {/** Tabs */}
-            <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
-                <div className="flex flex-1 items-center gap-2 md:gap-4">
-                    <TabButton name="Board" icon={<Grid3X3 className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
-                    <TabButton name="List" icon={<List className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
-                    <TabButton name="Timeline" icon={<Clock className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
-                    <TabButton name="Table" icon={<Table className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
+        <>
+            <ModalNewProject
+                isOpen={isModalNewProjectOpen}
+                onClose={() => setIsModalNewProjectOpen(false)}
+            />
+            <div className="px-4 xl:px-6">
+                <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+                    <Header
+                        name="Product Design Development"
+                        buttonComponent={
+                            <button
+                                className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+                                onClick={() => setIsModalNewProjectOpen(true)}
+                            >
+                                <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+                            </button>
+                        }
+                    />
                 </div>
-                <div className="flex items-center gap-2">
-                    <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-600">
-                        <Filter className="h-5 w-5" />
-                    </button>
-                    <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-600">
-                        <Share2 className="h-5 w-5" />
-                    </button>
-                    <div className="relative">
-                        <input type="text" placeholder="Search Task" className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white" />
-                        <Grid3x3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
+                {/** Tabs */}
+                <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
+                    <div className="flex flex-1 items-center gap-2 md:gap-4">
+                        <TabButton name="Board" icon={<Grid3X3 className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
+                        <TabButton name="List" icon={<List className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
+                        <TabButton name="Timeline" icon={<Clock className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
+                        <TabButton name="Table" icon={<Table className="h-5 w-5" />} activeTab={activeTab} setActiveTab={setActiveTab} />
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-600">
+                            <Filter className="h-5 w-5" />
+                        </button>
+                        <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-600">
+                            <Share2 className="h-5 w-5" />
+                        </button>
+                        <div className="relative">
+                            <input type="text" placeholder="Search Task" className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white" />
+                            <Grid3x3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
