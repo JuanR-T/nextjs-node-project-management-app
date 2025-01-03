79fb45790e5be2b707a29766a800c353aca017a5
JuanRT <juan.m13@hotmail.fr>
2025-01-03 22:34:06 +0100

    Add console.log statement and update image source URL

--- client/src/components/TaskCard/index.tsx
+++ client/src/components/TaskCard/index.tsx
@@ -7,6 +7,7 @@ type Props = {
 };
 
 const TaskCard = ({ task }: Props) => {
+    console.log(task, "task in listView");
     return (
         <div className="mb-3 rounded bg-white p-4 shadow dark:bg-dark-secondary dark:text-white">
             {task.attachments && task.attachments.length > 0 && (
@@ -15,7 +16,7 @@ const TaskCard = ({ task }: Props) => {
                     <div className="flex flex-wrap">
                         {task.attachments && task.attachments.length > 0 && (
                             <Image
-                                src={`https://pm-s3-images.s3.us-east-2.amazonaws.com/${task.attachments[0].fileURL}`}
+                                src={`/${task.attachments[0].fileURL}`}
                                 alt={task.attachments[0].fileName}
                                 width={400}
                                 height={200}
