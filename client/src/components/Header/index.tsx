b5334aab797200009435e14c3043bc50fef62c54
JuanRT <juan.m13@hotmail.fr>
2025-01-06 18:39:15 +0100

    Add simple-icons dependency and update Header component styles

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -11,8 +11,8 @@ const Header = ({
     isSmallText = false
 }: Props) => {
     return (
-        <div className="mb-5 flex w-full items-center justify-between">
-            <h1 className={`${isSmallText ? 'text-lg' : 'text-2xl'} px-2 font-semibold dark:text-white`}>
+        <div className="mb-5 justify-center flex w-full items-center ">
+            <h1 className={`${isSmallText ? 'text-lg' : 'text-2xl'} text-black px-2 font-semibold dark:text-white`}>
                 {name}
             </h1>
             {buttonComponent}
